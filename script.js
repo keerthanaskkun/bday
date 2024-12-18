6130e290be6e23ffe9c5e16cbe87eed4c7d86f93
keerthanaskkun <bk385217@gmail.com>
2024-12-19 00:18:21 +0530

    Initial commit: Added birthday website files

---
+++ script.js
@@ -0,0 +1,20 @@
+// Function to Release Balloons
+function releaseBalloons() {
+    alert('🎈 Balloons Released!');
+}
+
+// Function to Show Banner
+function showBanner() {
+    alert('🎉 Here is a special banner for you!');
+}
+
+// Function to Show Cake
+function showCake() {
+    alert('🎂 A delicious cake just for you!');
+}
+
+// Function to Play Favorite Songs
+function playFavoriteSongs() {
+    const song = new Audio('audio/song1.mp3');
+    song.play();
+}
